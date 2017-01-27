b907c4686798a86a9d7dc1b140055a976aa825c3
Lipegno <filp3q@gmail.com>
2017-01-27 13:34:31 +0000

    Data Aquisition Structure

---
+++ readModule.py
@@ -0,0 +1,17 @@
+import time
+
+class ADataAcquisition:
+    result = 0
+    readValue = 0
+    countSamples = 0
+    timestamp = time.time()
+
+    def __init__(self):
+        pass
+
+
+    def getDAQSamples_OUT(self):
+        pass
+
+    def addDAQSample(self):
+        pass
