2a7a7f9b4ffa76bbeed6ec46b6ff1f52184a38a3
Lipegno <filp3q@gmail.com>
2017-01-30 17:17:29 +0000

    Led Change Colors

--- ledModule.py
+++ ledModule.py
@@ -18,7 +18,7 @@ class LedController:
         Green color definition
         :return:
         """
-        self.pwmGreen.write(1.0000)
+        self.pwmGreen.write(0.5000)
         self.pwmRed.write(0.0000)
         self.pwmBlue.write(0.0000)
 
@@ -27,8 +27,8 @@ class LedController:
         Yellow color definition
         :return:
         """
-        self.pwmGreen.write(1.0000)
-        self.pwmRed.write(1.0000)
+        self.pwmGreen.write(0.5000)
+        self.pwmRed.write(0.5000)
         self.pwmBlue.write(0.0000)
 
     def redLed(self):
@@ -42,19 +42,27 @@ class LedController:
 
     def changeLeds(self,power):
         """
-        Changes the led color acording with the value
+        Changes the led color according with the value
         :param power:
         :return:
         """
         if power >= 1000:
             self.redLed()
-        elif power > 300 && power < 1000:
+        elif power > 300 and power < 1000:
             self.yellowLed()
+            #print("Yellow")
         else:
+            #print("Green")
             self.greenLed()
 
+"""
 if __name__ == "__main__":
     ledContoller = LedController(20,14,21)
-    power = 0
-    while power <= 4400:
-        ledContoller.changeLeds()
+
+    while True:
+        power = 0
+        while power <= 4400:
+            ledContoller.changeLeds(power)
+            power = power + 50
+            print("THe power is : " + str(power))
+"""
