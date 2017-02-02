9db1e9f81194c55dcf318968219f24171495ce5f
Lipegno <filp3q@gmail.com>
2017-02-02 15:01:12 +0000

    Leds changing colors properly

--- LEDFeedback/ledModule.py
+++ LEDFeedback/ledModule.py
@@ -1,5 +1,4 @@
 import mraa
-import time
 import threading
 
 class LedController(threading.Thread):
@@ -15,33 +14,97 @@ class LedController(threading.Thread):
         self.pwmBlue.enable(True)
         self.processedSamplesQueue = processedSamplesQueue
         self.processedSamplesQueueLock = processedSamplesQueueLock
+        # STATE CONSTANTS
+        self.CONST_STATE_GREEN = "green"
+        self.CONST_STATE_YELLOW = "yellow"
+        self.CONST_STATE_RED = "red"
+        #Leds State
+        self.currentState = self.CONST_STATE_GREEN
+        self.pastState = self.CONST_STATE_GREEN
+        #The color min value
+        self.CONST_GRADIENT = 0.10
 
-    def greenLed(self):
+        self.pinRed = 0.0000
+        self.pinBlue = 0.0000
+        self.pinGreen = 0.0000
+
+    def led(self,valueGreen, valueRed):
         """
-        Green color definition
+        Led color change
+        :param valueGreen:
+        :param valueRed:
         :return:
         """
-        self.pwmGreen.write(0.5000)
-        self.pwmRed.write(0.0000)
-        self.pwmBlue.write(0.0000)
+        self.pwmGreen.write(valueGreen)
+        self.pwmRed.write(valueRed)
 
-    def yellowLed(self):
+    def changeState(self, power):
         """
-        Yellow color definition
+        Will make the current state var according with the power received.
+        :param power:
         :return:
         """
-        self.pwmGreen.write(0.5000)
-        self.pwmRed.write(0.5000)
-        self.pwmBlue.write(0.0000)
+        self.pastState = self.currentState
+        if power >= 1000:
+            self.currentState = self.CONST_STATE_RED
+        elif power > 300 and power < 1000:
+            self.currentState = self.CONST_STATE_YELLOW
+        else:
+            self.currentState = self.CONST_STATE_GREEN
 
-    def redLed(self):
-        """
-        Red Color definition
-        :return:
-        """
-        self.pwmGreen.write(0.0000)
-        self.pwmRed.write(1.0000)
-        self.pwmBlue.write(0.0000)
+
+    def colorChange(self):
+        if self.currentState == self.CONST_STATE_RED:
+            if self.pastState == self.CONST_STATE_RED:
+                self.led(0.0000,1.0000)
+                self.pinRed = 1.0000
+                self.pinGreen = 0.0000
+            elif self.pastState == self.CONST_STATE_YELLOW:
+                self.pinGreen = self.pinGreen + self.CONST_GRADIENT
+                self.led(0.0000, 1.0000)
+                if self.pinGreen <= 0.0000 and self.pinRed >= 1.0000:
+                    self.pastState = self.CONST_STATE_RED
+            else:
+                self.pinRed = self.pinRed + self.CONST_GRADIENT
+                self.pinGreen = self.pinGreen - self.CONST_GRADIENT
+                self.led(self.pinGreen, self.pinRed)
+                if self.pinGreen <= 0.0000 and self.pinRed >= 1.0000:
+                    self.pastState = self.CONST_STATE_RED
+
+
+        if self.currentState == self.CONST_STATE_YELLOW:
+            if self.pastState == self.CONST_STATE_YELLOW:
+                self.led(1.0000,1.0000)
+                self.pinRed = 1.0000
+                self.pinGreen = 1.0000
+            elif self.pastState == self.CONST_STATE_RED:
+                self.pinGreen = self.pinGreen + self.CONST_GRADIENT
+                self.led(self.pinGreen, 1.0000)
+                if self.pinGreen >= 1.0000 and self.pinRed >= 1.0000:
+                    self.pastState = self.CONST_STATE_YELLOW
+            else :
+                self.pinRed = self.pinRed + self.CONST_GRADIENT
+                self.led(1.0000, self.pinRed)
+                if (self.pinGreen >= 1.0000 and self.pinRed >= 1.0000):
+                    self.pastState = self.CONST_STATE_YELLOW
+
+
+        if self.currentState == self.CONST_STATE_GREEN:
+            if self.pastState == self.CONST_STATE_GREEN:
+                self.led(1.0000,0.0000) #TODO PROBABLY NEED TO TI CHANFE THE GREEN VALUE AN NOT CHANGE IT SO NEED TO READ FROM THE PIN
+                self.pinRed = 0.0000
+                self.pinGreen = 1.0000
+            elif self.pastState == self.CONST_STATE_RED:
+                self.pinRed = self.pinRed - self.CONST_GRADIENT
+                self.pinGreen = self.pinGreen + self.CONST_GRADIENT
+                self.led(self.pinGreen,self.pinRed)
+                if self.pinGreen >= 1.0000 and self.pinRed <= 0.0000:
+                    self.pastState = self.CONST_STATE_GREEN
+            else:
+                self.pinRed = self.pinRed - self.CONST_GRADIENT
+                self.led(1.0000,self.pinRed)
+                if self.pinGreen >= 1.0000 and self.pinRed <= 0.0000:
+                    self.pastState = self.CONST_STATE_GREEN
 
     def run(self):
         """
@@ -49,21 +112,11 @@ class LedController(threading.Thread):
         :param power:
         :return:
         """
-        self.greenLed()
+        #Starts the plug with green power
         while True:
-            if not self.processedSamplesQueue.empty():
-                self.processedSamplesQueueLock.acquire()
-                power = self.processedSamplesQueue.get()["power"]
-                self.processedSamplesQueueLock.release()
-                if power >= 1000:
-                    self.redLed()
-                elif power > 300 and power < 1000:
-                    self.yellowLed()
-                    #print("Yellow")
-                else:
-                    self.greenLed()
-            else:
-                print("Don't need to change LED's")
+            power = 20
+            self.changeState(self, power)
+            self.colorChange()
 
 """
 if __name__ == "__main__":
@@ -72,7 +125,7 @@ if __name__ == "__main__":
     while True:
         power = 0
         while power <= 4400:
-            ledContoller.changeLeds(power)
-            power = power + 50
-            print("THe power is : " + str(power))
+            if ledContoller.currentState == ledContoller.CONST_NO_STATE:
+                ledContoller.greenLedTransition()
+                ledContoller.currentState = ledContoller.CONST_STATE_GREEN
 """
